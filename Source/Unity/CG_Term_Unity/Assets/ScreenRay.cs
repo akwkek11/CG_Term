8d0720fe877a8123c3b8dfe7e861e6a5e4d7f50c
xCC_Chob0 <akwkek11@naver.com>
2016-06-03 18:48:40 +0900

    Second Modification
    1. Test code added ( Get Touchpoint with raycast )
    2. Delete Background
    3. Magnification of object

---
+++ Source/Unity/CG_Term_Unity/Assets/ScreenRay.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenRay : MonoBehaviour
+{
+    public int clickLayer = 8;
+    public int blockLayer = 9;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("hit !!");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, 100f))
+            {
+                Debug.Log("hit point : " + hitInfo.point);
+
+                int l = hitInfo.transform.gameObject.layer;
+
+                if (l == clickLayer)
+                {
+                    Debug.Log(" hit object : " + hitInfo.collider.name);
+                }
+            }
+        }
+    }
+}
